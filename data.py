0748dbb66881fdc1952d83d7ea872a8d10bcc5ac
JS <js@JS.local>
2023-01-30 21:18:41 +0900

    varible and non_varible data

--- data.py
+++ data.py
@@ -1,9 +1,10 @@
-import data_class
+import varible_data_class
+import non_varible_data_class
 import json
-status = data_class.Status()
-location = data_class.Location()
-inventory = data_class.Inventory()
-quest_progress = data_class.QuestProgress()
+status = varible_data_class.Status()
+location = varible_data_class.Location()
+inventory = varible_data_class.Inventory()
+quest_progress = varible_data_class.QuestProgress()
 
 DATA = {"status" : status, "location" : location, "inventory" : inventory, "quest_progress" : quest_progress}
 
@@ -19,6 +20,6 @@ def load_data(filename):
             DATA[key].load(value)
     return DATA
 
-npc = data_class.NPC("save_npc")
+npc = non_varible_data_class.NPC("save_npc")
 print(npc.status)
 xxx = save_data("data_example")
