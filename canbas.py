a765465eb09fb0481177a6883f1a24eb981af5b5
JS <js@JS.local>
2023-02-03 21:50:01 +0900

    간단 클릭 시작

--- canbas.py
+++ canbas.py
@@ -14,26 +14,45 @@ town_back = pg.image.load("IMAGE/town_back_dark.png")
 wood_board = pg.image.load("IMAGE/wood_board.png")
 
 
-def board_and_write(x, y, string):
-    screen.blit(wood_board, (x,y))
-    font = font = pg.font.Font("font/NanumGothicBold.otf", 20)
-    write = font.render((string), True, (0,0,0))
-    screen.blit(write,(x+20, y+15))
-    return 0
+class BoardAndWrite:
+    def __init__(self, x, y, string):
+        self.wood_board_rect = wood_board.get_rect(topleft=(x, y))
+        screen.blit(wood_board, self.wood_board_rect.topleft)
+        font = pg.font.Font("font/NanumGothicBold.otf", 20)
+        self.write = font.render((string), True, (0,0,0))
+        self.text_rect = self.write.get_rect(topleft=(x+20, y+15))
+        screen.blit(self.write, self.text_rect.topleft)
+    def board_blit(self):
+        screen.blit(wood_board, self.wood_board_rect.topleft)
+        screen.blit(self.write, self.text_rect.topleft)
 
-def display_town():
-    screen.blit(town_back, (0,0))
-    board_and_write(100, 0, "레후")
-
-
+def display_town_button():   
+    b1 = BoardAndWrite(100, 50, "스테이터스를 보다")
+    b2 = BoardAndWrite(100, 110, "술집에 가다")
+    b3 = BoardAndWrite(100, 170, "상점에 가다")
+    b4 = BoardAndWrite(100, 170, "길드에 가다")
+    return [b1, b2, b3, b4]
+def button_cog(mouse_pos, buttons):
+    for bt in buttons:
+        if bt.wood_board_rect.collidepoint(mouse_pos):
+            return bt
+    return False
 
 running = True
 while running:
+    screen.blit(town_back, (0,0))
+    town_buttons = display_town_button()
     for event in pg.event.get():
+        mouse_pressed = pg.mouse.get_pressed()[0]
+        if mouse_pressed:
+            mouse_pos = pg.mouse.get_pos()
+            pressed = button_cog
+            if pressed:
+                print(pressed, 111)
         if event.type == pg.QUIT:
             running = False
     
-    display_town()
+    
     pg.display.update()
     
     clock.tick(60)
