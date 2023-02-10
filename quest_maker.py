9c8617bbac778d5aa54d719ed40fad2b967e2a1c
JS <js@JS.local>
2023-02-10 22:56:23 +0900

    퀘스트 랜덤 생성

--- quest_maker.py
+++ quest_maker.py
@@ -2,27 +2,26 @@ import json
 import random
 import os
 
-
-def json_to_dict(folder_path):
-    result = {}
-    for filename in os.listdir(folder_path):
-        if filename.endswith('.json'):
-            with open(os.path.join(folder_path, filename), 'r') as f:
-                result[filename[:-5]] = json.load(f)
-    return result
-def random_pick(dictionary):
-    x = random.choice(list(dictionary))
-    return dictionary[x]
-
-folder_path = './MONSTER/D'
-monsters_dict = json_to_dict(folder_path)
-pick = random_pick(monsters_dict)
-monster_name = pick["name"]
-monster_num = random.randint(1,4)
-print(pick)
-reward = pick["status"]["rank"]*10 + (pick["status"]["rank"]*3 *monster_num)
-
-quest_name = "%s %d 마리 퇴치 \n보상 %d 골드" % (monster_name, monster_num, reward)
+class random_monster_pick:
+    def __init__(self, folder_path):
+        self.folder_path = folder_path
+        monsters_dict = self.json_to_dict(self.folder_path)
+        pick = self.random_pick(monsters_dict)
+        self.monster_name = pick["name"]
+        self.monster_num = random.randint(1,4)
+        self.reward = pick["status"]["rank"]*10 + (pick["status"]["rank"]*3 *self.monster_num)
+        self.quest_name = "%s %d 마리 퇴치 \n보상 %d 골드" % (self.monster_name, self.monster_num, self.reward)
+    def json_to_dict(self, folder_path):
+        result = {}
+        for filename in os.listdir(folder_path):
+            if filename.endswith('.json'):
+                with open(os.path.join(folder_path, filename), 'r') as f:
+                    result[filename[:-5]] = json.load(f)
+        return result
+    def random_pick(self, dictionary):
+        x = random.choice(list(dictionary))
+        return dictionary[x]
 
 
-print(quest_name)
+x = random_monster_pick('./MONSTER/D')
+print(x.quest_name)
