8928ea3715b75e12f0a9efac9e896aade252713f
solitaryflyingbird <fkdlcb777@gmail.com>
2023-02-13 22:03:19 +0900

    공격 모듈화

--- battle.py
+++ battle.py
@@ -27,6 +27,8 @@ class Combat:
     def __init__(self, my_char, monsters):
         self.my_char = my_char
         self.monsters = monsters
+    def calculate_damage(self, attacker, defender):
+        return attacker.strength
 
     def simulate(self):
         battle_log = []
@@ -36,31 +38,39 @@ class Combat:
             for monster in self.monsters:
                 if monster.health <= 0:
                     continue
-                damage = monster.strength
+                damage = self.calculate_damage(monster, self.my_char)
                 self.my_char.health -= damage
                 battle_log += ["{}은(는) 공격했다. {}가 받은 데미지는 {}. {}의 남은 체력은 {}이다.".format(
                     monster.name, self.my_char.name, damage, self.my_char.name, self.my_char.health
                 )]
-
+                if self.my_char.health<=0:
+                    battle_log+=["{} 은(는) 쓰러졌다.".format(self.my_char.name)]
+            
+            if self.my_char.health <= 0 or any(monster.health <= 0 for monster in self.monsters):
+                break
             # my_char attacks a random monster
             alive_monsters = [monster for monster in self.monsters if monster.health > 0]
             if alive_monsters:
                 target = random.choice(alive_monsters)
-                damage = self.my_char.strength
+                damage = self.calculate_damage(self.my_char, target)
                 target.health -= damage
                 battle_log += ["{}은(는) 공격했다 {}가 받은 데미지는 {}. {}의 남은 체력은 {}이다.".format(
                     self.my_char.name, target.name, damage,  target.name, target.health
                 )]
                 if target.health<=0:
-                    battle_log+=["{} 은 쓰러졌다.".format(target.name)]
+                    battle_log+=["{} 은(는) 쓰러졌다.".format(target.name)]
 
         if self.my_char.health > 0:
-            battle_log += ["{} 는 전투에서 이겼다!".format(self.my_char.name)]
+            battle_log += ["{} 은(는) 전투에서 이겼다!".format(self.my_char.name)]
+            battle_result = 1
+
+        if self.my_char.health > 0:
+            battle_log += ["{} 은(는) 전투에서 이겼다!".format(self.my_char.name)]
             battle_result = 1
         else:
-            battle_log += ["{} 는 전투에서 패배했다!".format(self.my_char.name)]
+            battle_log += ["{} 은(는) 전투에서 패배했다!".format(self.my_char.name)]
             battle_result = 0
-        return (battle_log)
+        return (battle_log, battle_result)
 
 
 player = my_character("돌돌이", {'health': 100, 'strength': 12, 'dexterity': 10, 'intelligence': 10, 'drunk': False})
@@ -71,5 +81,5 @@ monsters = [monster("몬스터1", {'health': 20, 'strength': 8, 'dexterity': 8, 
 cc = Combat(player, monsters)
 xx = cc.simulate()
 
-for i in xx:
+for i in xx[0]:
     print(i)
